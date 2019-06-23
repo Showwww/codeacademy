b43c36717bb74ffd375ea0f8b39f27599de57ff6
Oleg <show@Olegs-MacBook-Air.local>
2019-06-23 13:01:44 +0300

    adding sleep date task

--- sleepDept.js
+++ sleepDept.js
@@ -1,12 +1,15 @@
+import 'console';
+
 const a = {
     'monday': 9,
      "tuesday": 10
   }
   
   function getSleepHours(day) {
-      return a[day];  
+      return a[day];
   }
   
   console.log(getSleepHours('monday'))
   console.log(getSleepHours('tuesday'))
   console.log(getSleepHours('monday'))
+  
