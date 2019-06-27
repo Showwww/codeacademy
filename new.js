6e2b5d81cc880e1bb9e775e7ea1e22847446d185
Oleg <show@Olegs-MacBook-Air.local>
2019-06-27 17:14:46 +0300

    mass

--- new.js
+++ new.js
@@ -1,3 +1,14 @@
-import 'console';
 
 console.log('Hello world' );
+let bobsFollowers = ['Joe', 'Marta', 'Sam', 'Erin'];
+let tinasFollowers = ['Sam', 'Marta', 'Elle'];
+let mutualFollowers = [];
+
+for (let i = 0; i < bobsFollowers.length; i++) {
+  for (let j = 0; j < tinasFollowers.length; j++) {
+    if (bobsFollowers[i] === tinasFollowers[j]) {
+      mutualFollowers.push(bobsFollowers[i]);
+      console.log(bobsFollowers[i]);
+    }
+  }
+};
